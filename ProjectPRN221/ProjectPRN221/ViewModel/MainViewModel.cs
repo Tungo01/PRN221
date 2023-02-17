4b34b46602d2b603d96cac2abf4fd64e26ee50d9
Tungo01 <tungmai2k1@gmail.com>
2023-02-17 22:20:42 +0700

    Login Window

---
+++ ProjectPRN221/ProjectPRN221/ViewModel/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ProjectPRN221.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjectPRN221.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            if (IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }
+        }
+    }
+}
