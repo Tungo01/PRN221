b5dc23c29039c5c43b65edef3bbfbb7b699b2f39
Tungo01 <tungmai2k1@gmail.com>
2023-03-04 17:05:21 +0700

    Cum Back

--- ProjectPRN221/ProjectPRN221/obj/Debug/net6.0-windows/View/LoginWindow.g.i.cs
+++ ProjectPRN221/ProjectPRN221/obj/Debug/net6.0-windows/View/LoginWindow.g.i.cs
@@ -65,7 +65,7 @@ namespace ProjectPRN221.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ProjectPRN221;V1.0.0.0;component/view/loginwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProjectPRN221;component/view/loginwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\LoginWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
