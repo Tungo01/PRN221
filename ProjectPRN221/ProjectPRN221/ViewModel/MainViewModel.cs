884e16e005c3e78cdea0881175761c9c2f9d28c8
Tungo01 <tungmai2k1@gmail.com>
2023-03-08 18:08:37 +0700

    Update UI

--- ProjectPRN221/ProjectPRN221/ViewModel/MainViewModel.cs
+++ ProjectPRN221/ProjectPRN221/ViewModel/MainViewModel.cs
@@ -5,20 +5,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace ProjectPRN221.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        public bool IsLoaded = false;
+        public ICommand UnitCommand { get; set; }
+        public ICommand SuplierCommand { get; set; }
+        public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
         public MainViewModel()
         {
-            if (IsLoaded)
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
-                IsLoaded = true;
-                LoginWindow loginWindow = new LoginWindow();
-                loginWindow.Show();
-            }
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+            SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow wd = new SuplierWindow();
+                wd.ShowDialog();
+            });
+            CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                CustomerWindow wd = new CustomerWindow();
+                wd.ShowDialog();
+            });
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow wd = new ObjectWindow();
+                wd.ShowDialog();
+            });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UserWindow wd = new UserWindow();
+                wd.ShowDialog();
+            });;
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                InputWindow wd = new InputWindow();
+                wd.ShowDialog();
+            });;
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                OutputWindow wd = new OutputWindow();
+                wd.ShowDialog();
+            });
         }
     }
 }
