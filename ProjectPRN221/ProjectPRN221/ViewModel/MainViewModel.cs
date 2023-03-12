b6c543f9eb4b13c06de869b45cbb7e3a7646bc8c
Tungo01 <tungmai2k1@gmail.com>
2023-03-12 14:11:10 +0700

    Ton Kho

--- ProjectPRN221/ProjectPRN221/ViewModel/MainViewModel.cs
+++ ProjectPRN221/ProjectPRN221/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@
 using ProjectPRN221.View;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -14,6 +15,8 @@ namespace ProjectPRN221.ViewModel
     public class MainViewModel : BaseViewModel
     {
         public bool Isloaded = false;
+        private ObservableCollection<TonKho> _TonKhoList;
+        public ObservableCollection<TonKho> TonKhoList { get => _TonKhoList; set { _TonKhoList = value; OnPropertyChanged(); } }
         public ICommand UnitCommand { get; set; }
         public ICommand SuplierCommand { get; set; }
         public ICommand CustomerCommand { get; set; }
@@ -25,6 +28,7 @@ namespace ProjectPRN221.ViewModel
 
         public MainViewModel()
         {
+            #region Show window Command
             UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
             {
                 UnitWindow wd = new UnitWindow();
@@ -60,6 +64,8 @@ namespace ProjectPRN221.ViewModel
                 OutputWindow wd = new OutputWindow();
                 wd.ShowDialog();
             });
+            #endregion
+
             LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
             {
                 if (p == null)
@@ -77,12 +83,46 @@ namespace ProjectPRN221.ViewModel
                 if (loginVM.IsLoggedIn)
                 {
                     p.Show();
+                    LoadTonKho();
                 }
                 else
                 {
                     p.Close();
                 }
             });
+
+            void LoadTonKho()
+            {
+                TonKhoList = new ObservableCollection<TonKho>();
+
+                var objectList = DataProvider.Ins.DB.Objects.ToList();
+                int i = 1;
+                foreach (var item in objectList)
+                {
+                    var inputList = DataProvider.Ins.DB.InputInfos.Where(p => p.IdObject == item.Id).ToList();
+                    var outputList = DataProvider.Ins.DB.OutputInfos.Where(p => p.IdObject == item.Id).ToList();
+
+                    int sumInput = 0;
+                    int sumOutput = 0;
+
+                    if (inputList != null)
+                    {
+                        sumInput = (int)inputList.Sum(p => p.Count);
+                    }
+                    if (outputList != null)
+                    {
+                        sumOutput = (int)outputList.Sum(p => p.Count);
+                    }
+
+                    TonKho tonKho= new TonKho();
+                    tonKho.STT = i;
+                    tonKho.Count = sumInput - sumOutput;
+                    tonKho.Object = item;
+
+                    TonKhoList.Add(tonKho);
+                    i++;
+                }
+            }
         }
     }
 }
